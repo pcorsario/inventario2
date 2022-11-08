cd8886a79ba170b57ce109fbe1489a5e1815d6d9
pcorsario <patriciovacaescbar@gmail.com>
2022-11-08 15:11:03 -0500

    INVENTARIO FIX

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,14 +10,14 @@
 	<title>@hasSection('title') @yield('title') | @endif {{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ secure_asset('js/app.js') }}" defer></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
 	 @livewireStyles
 </head>
 <body>
@@ -104,7 +104,7 @@
             @yield('content')
         </main>
     </div>
-    <script src="{{asset('js/index.js')}}"></script>
+    <script src="{{secure_asset('js/index.js')}}"></script>
     @livewireScripts
 <script type="text/javascript">
 	window.livewire.on('closeModal', () => {
